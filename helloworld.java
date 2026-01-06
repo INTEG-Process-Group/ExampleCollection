22a1c29897ffc0d468853e8b1564899eaf490037
Kevin Cloutier <81378386+jniordev@users.noreply.github.com>
2026-01-06 13:52:47 -0500

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class JavaApplication300 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
+
